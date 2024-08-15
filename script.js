cd32b53eaca4b5d3d670da12d4e57c0bc4b7b2e9
Ngang Fibel <ngangfibel@gmail.com>
2024-08-15 02:32:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const sideBar = document.querySelector('.sidebar');
+
+function closeSideBar(){
+    sideBar.style.display = 'none'
+}
+
+function openSideBar(){
+    sideBar.style.display = 'flex'
+}
